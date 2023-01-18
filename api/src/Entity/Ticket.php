f38cea2dbea27a155fa650ae0ee2aae9d971c681
Kurunchi CHANDRA <ckurunchi@gmail.com>
2023-01-18 10:21:44 +0100

    add api ressources on entity

--- api/src/Entity/Ticket.php
+++ api/src/Entity/Ticket.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TicketRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
+#[ApiResource]
 #[ORM\Entity(repositoryClass: TicketRepository::class)]
 class Ticket
 {
