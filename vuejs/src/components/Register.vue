5ba66d0e70e99b9df4ab1d02dfb56f99f35709ab
3kezoh <3kezoh@gmail.com>
2023-01-18 14:43:07 +0100

    fix: fix register

--- vuejs/src/components/Register.vue
+++ vuejs/src/components/Register.vue
@@ -1,172 +1,61 @@
-<template>
-  <div class="col-md-12">
-    <div class="card card-container">
-      <img
-        id="profile-img"
-        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-        class="profile-img-card"
-      />
-      <Form @submit="handleRegister" :validation-schema="schema">
-        <div v-if="!successful">
-          <div class="form-group">
-            <label for="lastname">Lastname</label>
-            <Field name="lastname" type="text" class="form-control" />
-            <ErrorMessage name="lastname" class="error-feedback" />
-          </div>
-          <div class="form-group">
-            <label for="firstname">Firstname</label>
-            <Field name="firstname" type="text" class="form-control" />
-            <ErrorMessage name="firstname" class="error-feedback" />
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <Field name="email" type="email" class="form-control" />
-            <ErrorMessage name="email" class="error-feedback" />
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <Field name="password" type="password" class="form-control" />
-            <ErrorMessage name="password" class="error-feedback" />
-          </div>
-
-          <div class="form-group">
-            <button class="btn btn-primary btn-block" :disabled="loading">
-              <span
-                v-show="loading"
-                class="spinner-border spinner-border-sm"
-              ></span>
-              Sign Up
-            </button>
-          </div>
-        </div>
-      </Form>
-
-      <div
-        v-if="message"
-        class="alert"
-        :class="successful ? 'alert-success' : 'alert-danger'"
-      >
-        {{ message }}
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import { Form, Field, ErrorMessage } from "vee-validate";
+<script setup>
+import DynamicForm from "./DynamicForm.vue";
 import * as yup from "yup";
 
-export default {
-  name: "Register",
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
-  data() {
-    const schema = yup.object().shape({
-      firstname: yup
+const schema = {
+  fields: [
+    {
+      label: "First name",
+      name: "firstname",
+      as: "input",
+      type: "text",
+      rules: yup.string().required("First name is required!"),
+    },
+    {
+      label: "Last Name",
+      name: "lastname",
+      as: "input",
+      type: "text",
+      rules: yup.string().required("Last name is required!"),
+    },
+    {
+      label: "Email",
+      name: "email",
+      as: "input",
+      type: "email",
+      rules: yup
         .string()
-        .required("Firstname is required!")
-        .min(3, "Must be at least 3 characters!")
-        .max(20, "Must be maximum 20 characters!"),
-        lastname: yup
-        .string()
-        .required("lastname is required!")
-        .min(3, "Must be at least 3 characters!")
-        .max(20, "Must be maximum 20 characters!"),
-      email: yup
-        .string()
-        .required("Email is required!")
         .email("Email is invalid!")
-        .max(50, "Must be maximum 50 characters!"),
-      password: yup
+        .max(50, "Must be maximum 50 characters!")
+        .required("Email is required!"),
+    },
+    {
+      label: "Password",
+      name: "password",
+      as: "input",
+      type: "password",
+      rules: yup
         .string()
-        .required("Password is required!")
         .min(6, "Must be at least 6 characters!")
-        .max(40, "Must be maximum 40 characters!"),
-    });
-
-    return {
-      successful: false,
-      loading: false,
-      message: "",
-      schema,
-    };
-  },
-  computed: {
-    loggedIn() {
-      return this.$store.state.auth.status.loggedIn;
+        .max(40, "Must be maximum 40 characters!")
+        .required("Password is required!"),
     },
-  },
-  mounted() {
-    if (this.loggedIn) {
-      this.$router.push("/profile");
-    }
-  },
-  methods: {
-    handleRegister(user) {
-      this.message = "";
-      this.successful = false;
-      this.loading = true;
-
-      this.$store.dispatch("auth/register", user).then(
-        (data) => {
-          this.message = data.message;
-          this.successful = true;
-          this.loading = false;
-        },
-        (error) => {
-          this.message =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-          this.successful = false;
-          this.loading = false;
-        }
-      );
-    },
-  },
+  ],
 };
+
+async function register(user) {
+  const response = await fetch("https://localhost/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  console.debug(response);
+}
 </script>
 
-<style scoped>
-label {
-  display: block;
-  margin-top: 10px;
-}
-
-.card-container.card {
-  max-width: 350px !important;
-  padding: 40px 40px;
-}
-
-.card {
-  background-color: #f7f7f7;
-  padding: 20px 25px 30px;
-  margin: 0 auto 25px;
-  margin-top: 50px;
-  -moz-border-radius: 2px;
-  -webkit-border-radius: 2px;
-  border-radius: 2px;
-  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-}
-
-.profile-img-card {
-  width: 96px;
-  height: 96px;
-  margin: 0 auto 10px;
-  display: block;
-  -moz-border-radius: 50%;
-  -webkit-border-radius: 50%;
-  border-radius: 50%;
-}
-
-.error-feedback {
-  color: red;
-}
-</style>
+<template>
+  <DynamicForm :schema="schema" :onSubmit="register" />
+</template>
