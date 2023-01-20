b48e36923ef06459c3170b97124adb2c2f81fdda
3kezoh <3kezoh@gmail.com>
2023-01-20 11:00:25 +0100

    fix: fix authentication redirection

--- vuejs/src/components/Home.vue
+++ vuejs/src/components/Home.vue
@@ -1,125 +1,147 @@
 <template>
-  <section class="bg-gray-900 text-white">
-    <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-      <div class="mx-auto max-w-3xl text-center">
-        <h1
-          class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-          Understand User Flow.
-          <span class="sm:block"> Increase Conversion. </span>
-        </h1>
+    <section class="bg-gray-900 text-white">
+        <div
+            class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+        >
+            <div class="mx-auto max-w-3xl text-center">
+                <h1
+                    class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                >
+                    Understand User Flow.
+                    <span class="sm:block"> Increase Conversion. </span>
+                </h1>
 
-        <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-          tenetur fuga ducimus numquam ea!
-        </p>
+                <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Nesciunt illo tenetur fuga ducimus numquam ea!
+                </p>
 
-        <div class="mt-8 flex flex-wrap justify-center gap-4">
-          <a class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-            href="/get-started">
-            Get Started
-          </a>
+                <div class="mt-8 flex flex-wrap justify-center gap-4">
+                    <a
+                        class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                        href="/get-started"
+                    >
+                        Get Started
+                    </a>
 
-          <a class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-            href="/about">Learn More
-          </a>
+                    <a
+                        class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                        href="/about"
+                        >Learn More
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
-  <section class="bg-gray-900 text-white">
-  <div>
-    <h2 class="text-xl font-medium text-center my-4">Actuellement au cinéma</h2>
-    <div class="flex flex-wrap justify-center">
-      <div class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md">
-        <img class="rounded-t-lg" alt="film poster" />
-        <div class="p-5">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Ant-man</h5>
-          <p class="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsa illo nisi aspernatur enim. Mollitia consequuntur 
-            vel natus quam obcaecati dicta corrupti reprehenderit pariatur quisquam itaque excepturi, officiis sequi in.</p>
-          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-            En savoir plus
-          </a>
+    </section>
+    <section class="bg-gray-900 text-white">
+        <div>
+            <h2 class="text-xl font-medium text-center my-4">
+                Actuellement au cinéma
+            </h2>
+            <div class="flex flex-wrap justify-center">
+                <div
+                    class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md"
+                >
+                    <img class="rounded-t-lg" alt="film poster" />
+                    <div class="p-5">
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            Ant-man
+                        </h5>
+                        <p class="mb-3 font-normal text-gray-700">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Magni ipsa illo nisi aspernatur enim. Mollitia
+                            consequuntur vel natus quam obcaecati dicta corrupti
+                            reprehenderit pariatur quisquam itaque excepturi,
+                            officiis sequi in.
+                        </p>
+                        <a
+                            href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        >
+                            En savoir plus
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md"
+                >
+                    <img class="rounded-t-lg" alt="film poster" />
+                    <div class="p-5">
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            Ant-man
+                        </h5>
+                        <p class="mb-3 font-normal text-gray-700">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Magni ipsa illo nisi aspernatur enim. Mollitia
+                            consequuntur vel natus quam obcaecati dicta corrupti
+                            reprehenderit pariatur quisquam itaque excepturi,
+                            officiis sequi in.
+                        </p>
+                        <a
+                            href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        >
+                            En savoir plus
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md"
+                >
+                    <img class="rounded-t-lg" alt="film poster" />
+                    <div class="p-5">
+                        <h5
+                            class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
+                        >
+                            Ant-man
+                        </h5>
+                        <p class="mb-3 font-normal text-gray-700">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Magni ipsa illo nisi aspernatur enim. Mollitia
+                            consequuntur vel natus quam obcaecati dicta corrupti
+                            reprehenderit pariatur quisquam itaque excepturi,
+                            officiis sequi in.
+                        </p>
+                        <a
+                            href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                        >
+                            En savoir plus
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md">
-        <img class="rounded-t-lg" alt="film poster" />
-        <div class="p-5">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Ant-man</h5>
-          <p class="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsa illo nisi aspernatur enim. Mollitia consequuntur 
-            vel natus quam obcaecati dicta corrupti reprehenderit pariatur quisquam itaque excepturi, officiis sequi in.</p>
-          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-            En savoir plus
-          </a>
-        </div>
-      </div>
-      <div class="max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow-md">
-        <img class="rounded-t-lg" alt="film poster" />
-        <div class="p-5">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Ant-man</h5>
-          <p class="mb-3 font-normal text-gray-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsa illo nisi aspernatur enim. Mollitia consequuntur 
-            vel natus quam obcaecati dicta corrupti reprehenderit pariatur quisquam itaque excepturi, officiis sequi in.</p>
-          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-            En savoir plus
-          </a>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  </section>
+    </section>
 </template>
 
-
-<script>
-import UserService from "../services/user.service";
-
-
-export default {
-  name: "Home",
-  data() {
-    return {
-      content: "",
-    };
-  },
-  mounted() {
-    UserService.getPublicContent().then(
-      (response) => {
-        this.content = response.data;
-      },
-      (error) => {
-        this.content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-      }
-    );
-  },
-};
-</script>
+<script></script>
 
 <style>
 .home-page {
-  text-align: center;
-  margin-top: 100px;
+    text-align: center;
+    margin-top: 100px;
 }
 
 header {
-  font-size: 40px;
-  font-weight: bold;
-  margin-bottom: 20px;
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 20px;
 }
 
 main {
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 20px;
 }
 
 h1 {
-  font-size: 400px;
-  font-weight: bold;
-  margin-bottom: 20px;
+    font-size: 400px;
+    font-weight: bold;
+    margin-bottom: 20px;
 }
 </style>
--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,17 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "./plugins";
 import App from "./App.vue";
 import router from "./router";
-
-// import "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./assets/main.css";
-
 import "./index.css";
+
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+
 app.component("font-awesome-icon", FontAwesomeIcon);
+
 app.mount("#app");
