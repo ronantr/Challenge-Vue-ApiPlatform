ee523425051c5fc9eedeb852da31665f2c577598
3kezoh <3kezoh@gmail.com>
2023-01-22 14:29:34 +0100

    feat: use post  to verify confirmation token

--- api/src/Controller/VerifyTokenController.php
+++ api/src/Controller/VerifyTokenController.php
@@ -25,8 +25,12 @@ class VerifyTokenController extends AbstractController
     $tokenRepository = $entityManager->getRepository(Token::class);
 
     $request = $this->requestStack->getCurrentRequest();
- 
-    $confirmationToken = $request->query->get('token');
+
+    $content = $request->getContent();
+
+    $data = json_decode($content, true);
+
+    $confirmationToken = $data['token'] ?? null;
 
     if (!$confirmationToken) {
       throw new UnprocessableEntityHttpException('Token is required');    
