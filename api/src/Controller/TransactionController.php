312085d964e9888f51b36df1c3580251d950137d
pthushanth <pthushanth97@gmail.com>
2023-02-05 00:22:48 +0100

    Add voter

--- api/src/Controller/TransactionController.php
+++ api/src/Controller/TransactionController.php
@@ -15,41 +15,29 @@ class TransactionController extends AbstractController
 {
     private $stripeService;
     private $levelService;
-    private $levelRepository;
 
-    public function __construct(StripeService $stripeService, UserLevelService $levelService, LevelRepository $levelRepository)
+    public function __construct(StripeService $stripeService, UserLevelService $levelService)
     {
         $this->stripeService = $stripeService;
         $this->levelService = $levelService;
-        $this->levelRepository = $levelRepository;
     }
 
     public function __invoke(Transaction $transaction)
     {
         $transaction->setStatus('success');
         $transaction->setDate(new \DateTime());
-
-            $this->levelService->updateUserLevel($transaction->getUser(), $transaction->getAmount());
         
-    
-//         ob_start();
-// dump($transaction);
-// $content = ob_get_clean();
-// $response = new Response($content);
-// $response->headers->set('Content-Type', 'text/plain');
-// return $response;
-
-        // try {
-        //     // Stripe payment logic
-        //     $charge=$this->stripeService->charge($transaction->getAmount() * 100, $transaction->getToken());
-        //     $transaction->setStatus($charge->status);
-        //     // Update user's points and level
-        //     $this->levelService->updateUserLevel($transaction->getUser(), $transaction->getAmount());
-        // } catch (\Exception $e) {
-        //     // Log the error
-        //     // ...
-        //     throw new \Exception('An error occurred while processing the transaction');
-        // }
-        return $transaction;
+        try {
+            // Stripe payment logic
+            $charge=$this->stripeService->charge($transaction->getAmount() * 100, $transaction->getToken());
+            $transaction->setStatus($charge->status);
+            // Update user's points and level
+            $this->levelService->updateUserLevel($transaction->getUser(), $transaction->getAmount());
+            return $transaction;
+        } catch (\Exception $e) {
+            // Log the error
+            // ...
+            throw new \Exception('An error occurred while processing the transaction');
+        }
     }
 }
