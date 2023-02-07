59d6612a7b1600e97325699375571a6a3a87c82e
3kezoh <3kezoh@gmail.com>
2023-02-07 02:01:03 +0100

    feat: users can upload files

---
+++ api/src/Entity/MediaObject.php
@@ -0,0 +1,69 @@
+<?php
+// api/src/Entity/MediaObject.php
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Controller\CreateMediaObjectController;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[Vich\Uploadable]
+#[ORM\Entity]
+#[ApiResource(
+normalizationContext: ['groups' => ['media_object:read']],
+types: ['https://schema.org/MediaObject'],
+operations: [
+new Get(),
+new GetCollection(),
+new Post(
+controller: CreateMediaObjectController::class,
+deserialize: false,
+validationContext: ['groups' => ['Default', 'media_object_create']],
+openapiContext: [
+'requestBody' => [
+'content' => [
+'multipart/form-data' => [
+'schema' => [
+'type' => 'object',
+'properties' => [
+'file' => [
+'type' => 'string',
+'format' => 'binary'
+]
+]
+]
+]
+]
+]
+]
+)
+]
+)]
+class MediaObject
+{
+  #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+  private ?int $id = null;
+
+  #[ApiProperty(types: ['https://schema.org/contentUrl'])]
+  #[Groups(['media_object:read'])]
+  public ?string $contentUrl = null;
+
+  #[Vich\UploadableField(mapping: "media_object", fileNameProperty: "filePath")]
+  #[Assert\NotNull(groups: ['media_object_create'])]
+  public ?File $file = null;
+
+  #[ORM\Column(nullable: true)]
+  public ?string $filePath = null;
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+}
--- api/src/Entity/TheaterGroup.php
+++ api/src/Entity/TheaterGroup.php
@@ -4,32 +4,110 @@ namespace App\Entity;
 
 use App\Repository\TheaterGroupRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Entity\User;
+use App\Entity\MediaObject;
 
+#[ApiResource(
+normalizationContext: ['groups' => [User::READ]],
+denormalizationContext: ['groups' => [User::WRITE]],
+)]
+#[Get]
+#[GetCollection]
+#[Post(
+uriTemplate: '/join',
+denormalizationContext: ['groups' => [TheaterGroup::WRITE]],
+
+)]
+#[Patch]
 #[ORM\Entity(repositoryClass: TheaterGroupRepository::class)]
 class TheaterGroup
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column()]
-    private ?int $id = null;
+  const READ = 'theaterGroup:read';
+  const WRITE = 'theaterGroup:write';
+  const PATCH = 'theaterGroup:patch';
 
-    #[ORM\OneToOne(inversedBy: 'theaterGroup', cascade: ['persist', 'remove'])]
-    private ?User $representative = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column()]
+  private ?int $id = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  #[ORM\OneToOne(inversedBy: 'theaterGroup', cascade: ['persist', 'remove'])]
+  private ?User $representative = null;
 
-    public function getRepresentative(): ?User
-    {
-        return $this->representative;
-    }
+  #[ORM\Column(options: ['default' => false])]
 
-    public function setRepresentative(?User $representative): self
-    {
-        $this->representative = $representative;
+  private ?bool $isVerified = false;
 
-        return $this;
-    }
+  #[ORM\Column(length: 255)]
+  #[Groups([TheaterGroup::READ, TheaterGroup::WRITE])]
+  private ?string $name = null;
+
+  #[ORM\Column(length: 255)]
+  #[Groups([TheaterGroup::READ, TheaterGroup::WRITE])]
+  private ?string $phoneNumber = null;
+
+  #[ORM\OneToOne(targetEntity: MediaObject::class)]
+  #[ORM\JoinColumn(nullable: true)]
+  #[ApiProperty]
+  public ?MediaObject $receipt = null;
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getRepresentative(): ?User
+  {
+    return $this->representative;
+  }
+
+  public function setRepresentative(?User $representative): self
+  {
+    $this->representative = $representative;
+
+    return $this;
+  }
+
+  public function isVerified(): ?bool
+  {
+    return $this->isVerified;
+  }
+
+  public function setIsVerified(bool $isVerified): self
+  {
+    $this->isVerified = $isVerified;
+
+    return $this;
+  }
+
+  public function getName(): ?string
+  {
+      return $this->name;
+  }
+
+  public function setName(string $name): self
+  {
+      $this->name = $name;
+
+      return $this;
+  }
+
+  public function getPhoneNumber(): ?string
+  {
+      return $this->phoneNumber;
+  }
+
+  public function setPhoneNumber(string $phoneNumber): self
+  {
+      $this->phoneNumber = $phoneNumber;
+
+      return $this;
+  }
 }
