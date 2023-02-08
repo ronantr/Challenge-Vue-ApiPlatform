7345bac1d69d91f6047c9f2814073446e4d2af9d
3kezoh <3kezoh@gmail.com>
2023-02-08 19:01:08 +0100

    feat: add 404 page

---
+++ vuejs/src/pages/Errors/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="not-found">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <p>
+            The page you are looking for might have been removed, had its name
+            changed, or is temporarily unavailable.
+        </p>
+        <router-link to="/">Go to Home</router-link>
+    </div>
+</template>
