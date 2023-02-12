30c0f3ca22eecf611044ae12ea2213abfa7446af
3kezoh <3kezoh@gmail.com>
2023-02-12 02:16:56 +0100

    feat: users can update event cover

--- vuejs/src/pages/TheaterGroup/Event.vue
+++ vuejs/src/pages/TheaterGroup/Event.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { object, string, date, number } from "yup";
 import { computed, ref, onMounted } from "vue";
 import { useToast } from "vue-toastification";
+import dayjs from "../../libs/day";
+import DynamicForm from "../../components/DynamicForm.vue";
 import { apiFetch } from "../../utils/apiFetch";
 
 const props = defineProps({
@@ -17,6 +20,8 @@ const props = defineProps({
 const event = ref(null);
 const isLoading = ref(true);
 const toast = useToast();
+const coverInput = ref(null);
+const cover = ref(null);
 
 onMounted(async () => {
     try {
@@ -34,14 +39,91 @@ const imageSrc = computed(
     () => import.meta.env.VITE_API_URL + event.value.contentUrl
 );
 
-async function updateEvent(fields) {
+const validationSchema = object({
+    name: string().min(3).max(255).required(),
+    date: date().required(),
+    location: string().required(),
+    description: string().max(2000).required(),
+    video: string().matches(
+        /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$|^$/,
+        "The url is not a vid youtube url"
+    ),
+    capacity: number().min(1).max(1000).required(),
+});
+
+const fields = [
+    {
+        label: "Name",
+        name: "name",
+        as: "input",
+        type: "text",
+    },
+    {
+        label: "Date",
+        name: "date",
+        as: "input",
+        type: "datetime-local",
+    },
+    {
+        label: "Location",
+        name: "location",
+        as: "input",
+        type: "text",
+    },
+    {
+        label: "Description",
+        name: "description",
+        as: "textarea",
+    },
+    {
+        label: "Capacity",
+        name: "capacity",
+        as: "input",
+        type: "number",
+    },
+    {
+        label: "Video",
+        name: "video",
+        as: "input",
+        type: "text",
+    },
+];
+
+async function updateEvent(payload) {
     try {
-        const { data } = await apiFetch("/events/" + props.eventId, fields, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/merge-patch+json",
-            },
-        });
+        const fields = {
+            ...payload,
+            ...(payload.date && {
+                date: dayjs(payload.date).utc().format("YYYY-MM-DDTHH:mm:ssZ"),
+            }),
+        };
+
+        const updatedFields = Object.keys(fields).reduce((acc, key) => {
+            if (fields[key] !== event.value[key]) {
+                acc[key] = fields[key];
+            }
+
+            return acc;
+        }, {});
+
+        const isAnyFieldUpdated = Object.keys(updatedFields).length > 0;
+
+        if (!isAnyFieldUpdated) {
+            toast.info("No changes to update");
+
+            return;
+        }
+
+        const { data } = await apiFetch(
+            "/events/" + props.eventId,
+            updatedFields,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/merge-patch+json",
+                },
+            }
+        );
 
         event.value = data;
 
@@ -54,6 +136,47 @@ async function updateEvent(fields) {
 const publish = () => updateEvent({ isPublished: true });
 
 const unpublish = () => updateEvent({ isPublished: false });
+
+const initialValues = computed(() => ({
+    ...event.value,
+    date: dayjs.utc(event.value.date).local().format("YYYY-MM-DDTHH:mm"),
+}));
+
+function deleteImage() {
+    coverInput.value.value = null;
+    cover.value = null;
+}
+
+async function updateImage() {
+    try {
+        if (!cover.value) {
+            toast.info("No image to update");
+
+            return;
+        }
+
+        const formData = new FormData();
+
+        formData.append("cover", cover.value);
+
+        const { data } = await apiFetch(
+            "/update_cover/" + props.eventId,
+            formData,
+            {
+                method: "POST",
+            }
+        );
+
+        event.value = data;
+
+        coverInput.value.value = null;
+        cover.value = null;
+
+        toast.success("Image updated");
+    } catch (error) {
+        toast.error("something went wrong");
+    }
+}
 </script>
 
 <template>
@@ -64,35 +187,22 @@ const unpublish = () => updateEvent({ isPublished: false });
         </router-link>
         <div class="flex flex-col gap-3">
             <img :src="imageSrc" alt="event cover" />
+            <div class="flex flex-row gap-1">
+                <input
+                    type="file"
+                    ref="coverInput"
+                    @change="cover = $event.target.files[0]"
+                />
+                <div v-if="cover" class="flex flex-col gap-1">
+                    <button v-show="cover" @click="updateImage">Update</button>
+                    <button @click="deleteImage">Cancel</button>
+                </div>
+            </div>
             <div v-if="event.isPublished" class="flex flex-col gap-1">
                 <span>Published</span>
             </div>
             <div v-else class="flex flex-col gap-1">
                 <span>Not published</span>
-            </div>
-            <div class="flex flex-col gap-1">
-                <span>Name</span>
-                <span>{{ event.name }}</span>
-            </div>
-            <div class="flex flex-col gap-1">
-                <span>Date</span>
-                <span>{{ event.date }}</span>
-            </div>
-            <div class="flex flex-col gap-1">
-                <span>Description</span>
-                <span>{{ event.description }}</span>
-            </div>
-            <div class="flex flex-col gap-1">
-                <span>Capacity</span>
-                <span>{{ event.capacity }}</span>
-            </div>
-            <div class="flex flex-col gap-1">
-                <span>Location</span>
-                <span>{{ event.location }}</span>
-            </div>
-            <div v-if="event.video" class="flex flex-col gap-1">
-                <span>Video</span>
-                <span>{{ event.video }}</span>
             </div>
             <button
                 v-if="event.isPublished"
@@ -105,5 +215,11 @@ const unpublish = () => updateEvent({ isPublished: false });
                 Publish
             </button>
         </div>
+        <DynamicForm
+            :fields="fields"
+            :validationSchema="validationSchema"
+            :initial-values="initialValues"
+            @submit="updateEvent"
+        />
     </section>
 </template>
